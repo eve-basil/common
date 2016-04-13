f2cd1d531a1cefdc5da4b418c866be0d76aa349b
Steve Lewis <stevelle@gmail.com>
2016-04-12 22:39:57 -0700

    Add url join which serves our needs
    Existing functions in common libraries add extra slashes.

--- basil_common/str_support.py
+++ basil_common/str_support.py
@@ -5,3 +5,12 @@ def as_int(value):
         return int(value)
     except ValueError:
         return None
+
+
+def urljoin(*parts):
+    url = parts[0]
+    for p in parts[1:]:
+        if url[-1] != '/':
+            url += '/'
+        url += p
+    return url
