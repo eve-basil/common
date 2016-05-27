6f50c14c904542dc391c26ba28a548ddb510a463
Steve Lewis <stevelle@gmail.com>
2016-05-27 16:45:22 -0700

    remove unneeded LOG function
    This just made my logging be less useful

--- basil_common/__init__.py
+++ basil_common/__init__.py
@@ -4,8 +4,3 @@ logging.basicConfig(
     format='[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] '
            '%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S +0000', level=logging.DEBUG)
-LOG = logging.getLogger(__name__)
-
-
-def logger():
-    return LOG
